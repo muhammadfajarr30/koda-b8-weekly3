5725eefa304a122660ab95cd147dc611de4818d6
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-07-10 15:01:42 +0700

    feat: enhance Home and DisplayCategory functions for improved menu selection and cart management

--- ui/home.go
+++ ui/home.go
@@ -4,50 +4,127 @@ import (
 	"fmt"
 	"koda-b8-weekly3/services"
 	"koda-b8-weekly3/utils"
+	"os"
+	"strconv"
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
 
 func Home() {
-	p := message.NewPrinter(language.Indonesian)
-	// menus := services.GetMenu()
-	fmt.Println("===== SELAMAT DATANG DI GACOAN =====")
-	fmt.Println("1. MAKANAN")
-	fmt.Println("2. MINUMAN")
-	fmt.Println("3. DIMSUM")
-	fmt.Println("====================================")
-	input := utils.Input("Masukkan Input: ")
 
+	fmt.Println("========SELAMAT DATANG DI GACOAN ========")
+	fmt.Println("1. Order Makanan")
+	fmt.Println("2. Checkout")
+	fmt.Println("------------------------------------------")
+	fmt.Println("0 Exit")
+
+	input := utils.Input("Pilih Menu: ")
 	switch input {
 	case "1":
-		utils.Clear()
-		fmt.Println("======= MAKANAN =======")
-		foods := services.GetMenuByCategory("Makanan")
-		for idx, menu := range foods {
-			if menu.Category == "Makanan" {
-				p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
-			}
-		}
-
+		DisplayCategory()
 	case "2":
-		utils.Clear()
-		fmt.Println("======= MINUMAN =======")
-		drinks := services.GetMenuByCategory("Minuman")
-		for idx, menu := range drinks {
-			if menu.Category == "Minuman" {
-				p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
-			}
-		}
 
-	case "3":
+	case "0":
+		os.Exit(0)
+	}
+
+}
+
+func DisplayCart() {
+
+}
+
+func DisplayCategory() {
+	p := message.NewPrinter(language.Indonesian)
+	for {
 		utils.Clear()
-		fmt.Println("======= DIMSUM =======")
-		sideDish := services.GetMenuByCategory("Dimsum")
-		for idx, menu := range sideDish {
-			if menu.Category == "Dimsum" {
-				p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
+		fmt.Println("===== SILAKAN PILIH KATEGORI =====")
+		fmt.Println("1. MAKANAN")
+		fmt.Println("2. MINUMAN")
+		fmt.Println("3. DIMSUM")
+		fmt.Println("====================================")
+		input := utils.Input("Masukkan Input: ")
+
+		switch input {
+		case "1":
+			utils.Clear()
+			fmt.Println("======= MAKANAN =======")
+			foods := services.GetMenuByCategory("Makanan")
+			for idx, menu := range foods {
+				if menu.Category == "Makanan" {
+					p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
+				}
 			}
+			inputMenu := utils.Input("Pilih menu : ")
+			menuIndex, err := strconv.Atoi(inputMenu)
+			if err != nil {
+				fmt.Println("Input harus berupa angka")
+				return
+			}
+			if menuIndex < 1 || menuIndex > len(foods) {
+				fmt.Println("Pilh sesuai dengan menu yang ada")
+			}
+			selectedMenu := foods[menuIndex-1]
+			qtyInput := utils.Input("Jumlah: ")
+			qty, err := strconv.Atoi(qtyInput)
+			if err != nil {
+				fmt.Print("Jumlah harus berupa angka")
+			}
+			services.AddToCart(selectedMenu, qty)
+		case "2":
+			utils.Clear()
+			fmt.Println("======= MINUMAN =======")
+			drinks := services.GetMenuByCategory("Minuman")
+			for idx, menu := range drinks {
+				if menu.Category == "Minuman" {
+					p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
+				}
+			}
+			inputMenu := utils.Input("Pilih menu : ")
+			menuIndex, err := strconv.Atoi(inputMenu)
+			if err != nil {
+				fmt.Println("Input harus berupa angka")
+				return
+			}
+			if menuIndex < 1 || menuIndex > len(drinks) {
+				fmt.Println("Pilh sesuai dengan menu yang ada")
+			}
+			selectedMenu := drinks[menuIndex-1]
+			qtyInput := utils.Input("Jumlah: ")
+			qty, err := strconv.Atoi(qtyInput)
+			if err != nil {
+				fmt.Print("Jumlah harus berupa angka")
+			}
+			services.AddToCart(selectedMenu, qty)
+		case "3":
+			utils.Clear()
+			fmt.Println("======= DIMSUM =======")
+			sideDish := services.GetMenuByCategory("Dimsum")
+			for idx, menu := range sideDish {
+				if menu.Category == "Dimsum" {
+					p.Printf("%d. %s - Rp.%d\n", idx+1, menu.Name, menu.Price)
+				}
+			}
+			inputMenu := utils.Input("Pilih menu : ")
+			menuIndex, err := strconv.Atoi(inputMenu)
+			if err != nil {
+				fmt.Println("Input harus berupa angka")
+				return
+			}
+			if menuIndex < 1 || menuIndex > len(sideDish) {
+				fmt.Println("Pilh sesuai dengan menu yang ada")
+			}
+			selectedMenu := sideDish[menuIndex-1]
+			qtyInput := utils.Input("Jumlah: ")
+			qty, err := strconv.Atoi(qtyInput)
+			if err != nil {
+				fmt.Print("Jumlah harus berupa angka")
+			}
+			services.AddToCart(selectedMenu, qty)
+
+		default:
+			fmt.Println("Pilih sesuai menu")
 		}
 	}
 }
