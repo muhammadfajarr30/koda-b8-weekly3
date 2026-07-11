215a6a21faac520ef46b3e60531a2151e15e01e2
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-07-11 23:11:19 +0700

    feat: implement quantity update for existing cart items in AddToCart function

--- services/cart.go
+++ services/cart.go
@@ -7,6 +7,12 @@ import (
 var CartItems []models.Cart
 
 func AddToCart(menu models.Menu, qty int) {
+	for i := range CartItems {
+		if CartItems[i].Menu.ID == menu.ID {
+			CartItems[i].Quantity += qty
+			return
+		}
+	}
 	item := models.Cart{
 		Menu:     menu,
 		Quantity: qty,
